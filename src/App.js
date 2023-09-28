159abccccd89656af8b0b603adadbaf840c2cb9b
aLexx Sub <alexei.s@mail.ru>
2023-09-28 10:21:55 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import './style.css'
 class PhoneBook extends Component {
   constructor(props) {
     super(props);
-
+    // состояние компонента
     this.state = { phones: [], id:null,number:'',name: '',editmode:false };
-    this.handleChangeName = this.handleChangeName.bind(this);
-    this.handleChangeNumber = this.handleChangeNumber.bind(this);
+    //методы компонента
     this.setPhone = this.setPhone.bind(this);
     this.deletePhone = this.deletePhone.bind(this);
     this.resetPhone = this.resetPhone.bind(this);
     this.savePhone = this.savePhone.bind(this);
   }
-
+// метод отрисовки, возвращает документ
   render() {
     return (
       <div>                   
@@ -24,14 +23,14 @@ class PhoneBook extends Component {
               <input
                   ref={(input) => { this.inputnumber = input; }} 
                   placeholder="Введите номер"
-                  onChange={this.handleChangeNumber}
+                  onChange={e =>  this.setState({ number: e.target.value })}
                   value={this.state.number}
                 />
           </td>
           <td>
               <input
                 placeholder="Введите имя"
-                onChange={this.handleChangeName}
+                onChange={e => this.setState({ name: e.target.value })}
                 value={this.state.name}
                 onKeyUp={this.handleKeyUp}
               />
@@ -55,6 +54,11 @@ class PhoneBook extends Component {
       </div>
     );
   }
+// хук на монтаж компонента на страницу, подменяем заголовок приложения
+  componentDidMount(){
+    document.title = "SPA PhoneBook"
+  }
+// сброс параметров редактирования
   resetPhone(){
     this.setState({ 
       id:null,
@@ -63,6 +67,7 @@ class PhoneBook extends Component {
       editmode:false });
       this.inputnumber.focus()
   }
+  // сохраняем новые данные
   savePhone(){
 
     this.setState(state => ({
@@ -76,7 +81,7 @@ class PhoneBook extends Component {
     }));
     this.resetPhone()
   }
-
+// устанавливаем данные при редактировании
   setPhone(phone) {
     this.setState({ 
       id:phone.id, 
@@ -86,7 +91,7 @@ class PhoneBook extends Component {
     
     
   }
-
+// удаляем запись 
   deletePhone(id) {
     this.setState(state => ({
       phones: this.state.phones.filter(phone => 
@@ -94,13 +99,7 @@ class PhoneBook extends Component {
       )
     }));
   }
-  handleChangeName(e) {
-    this.setState({ name: e.target.value });
-  }
-  handleChangeNumber(e) {
-    this.setState({ number: e.target.value });
-  }
-  
+// отрабатываем нажатие энтера в поле имя, по событию добавляем запись.
   handleKeyUp = (event) => {
     if(event.key === 'Enter'){
       if (!this.state.name.length) {
@@ -125,7 +124,7 @@ class PhoneBook extends Component {
 
 
 }
-
+// дополнительный компонент - список телевонной книги
 class PhonesList extends Component {
 
   render() {
