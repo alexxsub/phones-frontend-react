0326cc6c676d7b03e58de445c3f33c813a8e1d7f
alexx sub <alexei.s@list.ru>
2023-10-03 18:38:40 +0300

    fn phonelist

--- src/App.js
+++ src/App.js
@@ -1,155 +1,156 @@
-import {Component} from 'react';
-import './style.css'
+import { Component } from "react";
+import "./style.css";
 
 class PhoneBook extends Component {
   constructor(props) {
     super(props);
     // состояние компонента
-    this.state = { phones: [], id:null,number:'',name: '',editmode:false };
+    this.state = {
+      phones: [],
+      id: null,
+      number: "",
+      name: "",
+      editmode: false,
+    };
     //методы компонента
     this.setPhone = this.setPhone.bind(this);
     this.deletePhone = this.deletePhone.bind(this);
     this.resetPhone = this.resetPhone.bind(this);
     this.savePhone = this.savePhone.bind(this);
   }
-// метод отрисовки, возвращает документ
+  // метод отрисовки, возвращает документ
   render() {
     return (
-      <div>                   
-       <table>
-        <tbody>
-          <tr>
-            <td>
-              <input
-                  ref={(input) => { this.inputnumber = input; }} 
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <input
+                  ref={(input) => {
+                    this.inputnumber = input;
+                  }}
                   placeholder="Введите номер"
-                  onChange={e =>  this.setState({ number: e.target.value })}
+                  onChange={(e) => this.setState({ number: e.target.value })}
                   value={this.state.number}
                 />
-          </td>
-          <td>
-              <input
-                placeholder="Введите имя"
-                onChange={e => this.setState({ name: e.target.value })}
-                value={this.state.name}
-                onKeyUp={this.handleKeyUp}
-              />
-          </td>
-          <td>
-              {/* <button style={{ 'display': this.state.editmode ? 'block' : 'none' }} onClick={this.savePhone}>Сохранить</button> */}
-              {this.state.editmode && <button onClick={this.savePhone}>Сохранить</button>}
-          </td>
-          <td>
-              {/* <button style={{ 'display': this.state.editmode ? 'block' : 'none' }} onClick={this.resetPhone}>Отмена</button> */}
-              {this.state.editmode &&<button  onClick={this.resetPhone}>Отмена</button>}
-          </td>
-      </tr>
-   </tbody>
-   </table> 
-        <PhonesList 
-              phones={this.state.phones}
-              setPhone={this.setPhone}
-              deletePhone={this.deletePhone}
+              </td>
+              <td>
+                <input
+                  placeholder="Введите имя"
+                  onChange={(e) => this.setState({ name: e.target.value })}
+                  value={this.state.name}
+                  onKeyUp={this.handleKeyUp}
+                />
+              </td>
+              <td>
+                {/* <button style={{ 'display': this.state.editmode ? 'block' : 'none' }} onClick={this.savePhone}>Сохранить</button> */}
+                {this.state.editmode && (
+                  <button onClick={this.savePhone}>Сохранить</button>
+                )}
+              </td>
+              <td>
+                {/* <button style={{ 'display': this.state.editmode ? 'block' : 'none' }} onClick={this.resetPhone}>Отмена</button> */}
+                {this.state.editmode && (
+                  <button onClick={this.resetPhone}>Отмена</button>
+                )}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <PhonesList
+          phones={this.state.phones}
+          setPhone={this.setPhone}
+          deletePhone={this.deletePhone}
         />
       </div>
     );
   }
-// хук на монтаж компонента на страницу, подменяем заголовок приложения
-  componentDidMount(){
-    document.title = "SPA PhoneBook"
+  // хук на монтаж компонента на страницу, подменяем заголовок приложения
+  componentDidMount() {
+    document.title = "SPA PhoneBook";
   }
-// сброс параметров редактирования
-  resetPhone(){
-    this.setState({ 
-      id:null,
-      number: '',
-      name: '',
-      editmode:false });
-      this.inputnumber.focus()
+  // сброс параметров редактирования
+  resetPhone() {
+    this.setState({
+      id: null,
+      number: "",
+      name: "",
+      editmode: false,
+    });
+    this.inputnumber.focus();
   }
   // сохраняем новые данные
-  savePhone(){
-
-    this.setState(state => ({
-      phones: this.state.phones.map(phone => {
-       if (phone.id===this.state.id) {
-        phone.number=this.state.number
-        phone.name=this.state.name
-       } 
-       return phone
-    })
+  savePhone() {
+    this.setState((state) => ({
+      phones: this.state.phones.map((phone) => {
+        if (phone.id === this.state.id) {
+          phone.number = this.state.number;
+          phone.name = this.state.name;
+        }
+        return phone;
+      }),
     }));
-    this.resetPhone()
+    this.resetPhone();
   }
-// устанавливаем данные при редактировании
+  // устанавливаем данные при редактировании
   setPhone(phone) {
-    this.setState({ 
-      id:phone.id, 
+    this.setState({
+      id: phone.id,
       number: phone.number,
       name: phone.name,
-      editmode:true });
-    
-    
+      editmode: true,
+    });
   }
-// удаляем запись 
+  // удаляем запись
   deletePhone(id) {
-    this.setState(state => ({
-      phones: this.state.phones.filter(phone => 
-       phone.id!==id
-      )
+    this.setState((state) => ({
+      phones: this.state.phones.filter((phone) => phone.id !== id),
     }));
   }
-// отрабатываем нажатие энтера в поле имя, по событию добавляем запись.
+  // отрабатываем нажатие энтера в поле имя, по событию добавляем запись.
   handleKeyUp = (event) => {
-    if(event.key === 'Enter'){
+    if (event.key === "Enter") {
       if (!this.state.name.length) {
         return;
       }
       const newItem = {
         id: Date.now(),
         number: this.state.number,
-        name: this.state.name
-        
+        name: this.state.name,
       };
-      this.setState(state => ({
+      this.setState((state) => ({
         phones: state.phones.concat(newItem),
-        id:null,
-        name: '',
-        number:''
+        id: null,
+        name: "",
+        number: "",
       }));
-      this.inputnumber.focus()
+      this.inputnumber.focus();
     }
-    }
-  
-
-
+  };
 }
-// дополнительный компонент - список телевонной книги
-class PhonesList extends Component {
-
-  render() {
-    
-    return (
-  
- <table width="350px">
-  <tbody>
-   {this.props.phones.map(phone => (
-          <tr key={phone.id}>          
-          {/* eslint-disable-next-line  */}
-          <td><a href="#" onClick={() => this.props.setPhone(phone)} >{ phone.number }</a></td>
-          <td>{ phone.name }</td>
-          <td><button onClick={() => this.props.deletePhone(phone.id)}> x</button></td>
-        </tr>
+// дополнительный компонент - список телефонной книги
+function PhonesList(props) {
+  return (
+    <table width="350px">
+      <tbody>
+        {props.phones.map((phone) => (
+          <tr key={phone.id}>
+            <td>
+              {/* eslint-disable-next-line  */}
+              <a href="#" onClick={() => props.setPhone(phone)}>
+                {phone.number}
+              </a>
+            </td>
+            <td>{phone.name}</td>
+            <td>
+              <button onClick={() => props.deletePhone(phone.id)}> x</button>
+            </td>
+          </tr>
         ))}
-
-</tbody>
-
-</table>
-
-    );
-  }
-
-
+      </tbody>
+    </table>
+  );
 }
 
 export default PhoneBook;
